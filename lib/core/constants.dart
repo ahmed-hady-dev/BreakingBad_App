289ad4ec1e7160492683d392cc82107de946ab5d
Ahmed  Abd ElHady <52330103+ahmed-hady-dev@users.noreply.github.com>
2021-08-19 12:33:34 +0200

    #4 Home layout and search function added

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -3,8 +3,8 @@ import 'package:flutter/cupertino.dart';
 const String baseUrl = 'https://www.breakingbadapi.com/api/';
 
 class MyColors {
-  static const Color lightBlue = Color(0xFF007FFF);
-  static const Color darkBlue = Color(0xFF17234D);
+  static const Color lightBlue = Color(0xFF8ECAE6);
+  static const Color darkBlue = Color(0xFF023047);
   static const Color white = Color(0xFFF6FAFB);
   static const Color lightGreen = Color(0xFF17E8E2);
 }
--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -1,9 +1,12 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:breakingbad_app/bloc/characters_cubit.dart';
+import 'package:breakingbad_app/core/constants.dart';
+import 'package:breakingbad_app/core/router/router.dart';
+import 'package:breakingbad_app/model/character_model.dart';
+import 'package:breakingbad_app/view/widgets/loading_widget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_conditional_rendering/conditional.dart';
 
 class HomeView extends StatefulWidget {
   @override
@@ -11,6 +14,50 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  bool isSearch = false;
+  List<CharacterModel> searchedCharactersList = [];
+  TextEditingController searchController = TextEditingController();
+
+  void addSearchedForItemsToSearchedList(String searchedCharacters) {
+    searchedCharactersList = CharactersCubit.get(context)
+        .list
+        .where((charachter) =>
+            charachter.name.toLowerCase().startsWith(searchedCharacters))
+        .toList();
+    setState(() {});
+  }
+
+  Widget buildSearchField() {
+    return TextField(
+      controller: searchController,
+      cursorColor: MyColors.lightBlue,
+      decoration: InputDecoration(
+        hintText: 'Find a character...',
+        border: InputBorder.none,
+        hintStyle: TextStyle(color: MyColors.lightBlue, fontSize: 18),
+      ),
+      style: TextStyle(color: MyColors.lightBlue, fontSize: 18),
+      onChanged: (searchedCharacter) {
+        addSearchedForItemsToSearchedList(searchedCharacter);
+      },
+    );
+  }
+
+  void _startSearch() {
+    ModalRoute.of(context)!
+        .addLocalHistoryEntry(LocalHistoryEntry(onRemove: _stopSearching));
+    setState(() {
+      isSearch = true;
+    });
+  }
+
+  void _stopSearching() {
+    setState(() {
+      searchController.clear();
+      isSearch = false;
+    });
+  }
+
   @override
   void initState() {
     CharactersCubit.get(context).getCharacters();
@@ -22,23 +69,90 @@ class _HomeViewState extends State<HomeView> {
     return BlocBuilder<CharactersCubit, CharactersState>(
       builder: (context, state) {
         var characterCubit = CharactersCubit.get(context);
+        var characterGetter =
+            isSearch ? searchedCharactersList : characterCubit.list;
         return SafeArea(
           child: Scaffold(
+            appBar: AppBar(
+              backgroundColor: MyColors.darkBlue,
+              title: isSearch
+                  ? buildSearchField()
+                  : Text('Breaking Bad Characters'),
+              centerTitle: true,
+              actions: isSearch
+                  ? [
+                      IconButton(
+                          onPressed: () {
+                            _stopSearching();
+                            MagicRouter.pop();
+                          },
+                          icon: Icon(Icons.close))
+                    ]
+                  : [
+                      IconButton(
+                          onPressed: _startSearch,
+                          icon: Icon(Icons.search_rounded))
+                    ],
+            ),
             body: characterCubit.isLoading
-                ? Center(
-                    child: CupertinoActivityIndicator(
-                    animating: true,
-                    radius: 24,
-                  ))
-                : ListView.builder(
-                    itemCount: characterCubit.list.length,
-                    shrinkWrap: true,
-                    padding: const EdgeInsets.all(16),
-                    itemBuilder: (context, index) => Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Center(
+                ? LoadingWidget()
+                : GridView.builder(
+                    itemCount: searchController.text.isEmpty
+                        ? characterGetter.length
+                        : searchedCharactersList.length,
+                    // padding: EdgeInsets.all(24),
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 1,
+                      crossAxisSpacing: 1,
+                      childAspectRatio: 2 / 3,
+                    ),
+                    itemBuilder: (context, index) => Container(
+                      width: double.infinity,
+                      margin: EdgeInsetsDirectional.fromSTEB(8, 8, 8, 8),
+                      padding: EdgeInsetsDirectional.all(4),
+                      decoration: BoxDecoration(
+                        color: MyColors.lightBlue,
+                        borderRadius: BorderRadius.circular(24),
+                      ),
+                      child: GridTile(
+                        child: characterGetter[index].image.isNotEmpty
+                            ? ClipRRect(
+                                borderRadius: BorderRadius.circular(24),
+                                child: FadeInImage.assetNetwork(
+                                  width: double.infinity,
+                                  height: double.infinity,
+                                  placeholder: 'assets/images/loading.gif',
+                                  image: characterGetter[index].image,
+                                  fit: BoxFit.cover,
+                                ),
+                              )
+                            : Image.asset('assets/images/placeholder.png'),
+                        footer: Container(
+                          width: double.infinity,
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 15, vertical: 10),
+                          // color: Colors.black54,
+                          decoration: BoxDecoration(
+                            color: MyColors.lightBlue,
+                            borderRadius: BorderRadius.circular(24),
+                          ),
+                          alignment: Alignment.bottomCenter,
                           child: Text(
-                              characterCubit.list[index].charId.toString())),
+                            '${characterGetter[index].name}',
+                            style: TextStyle(
+                              height: 1.3,
+                              fontSize: 16,
+                              color: MyColors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 2,
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
                     ),
                   ),
           ),
